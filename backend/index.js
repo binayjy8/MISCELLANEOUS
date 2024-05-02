503566c360a33d07e8587b5f00ed888c0a0a7d0f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-02 15:55:25 +0530

    code backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.post("/register", (req, res) => {
     res.send("standard POST response");
 });
