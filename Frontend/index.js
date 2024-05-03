857bb64e3a245976c618d285f31edb7d3ac28287
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-03 13:16:12 +0530

    code backend

--- Frontend/index.js
+++ Frontend/index.js
@@ -7,16 +7,27 @@
 //     console.log(`hi i am ${this.name}`);
 // }
 
-class Person {
-    constructor(name, age) {
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     talk() {
+//         console.log(`hi i am ${this.name}`);
+//     }
+// }
+
+// let p1 = new Person ("Gillims", 69);
+// let p2 = new Person ("Bloom", 12);
+
+class Student {
+    constructor(name, age, marks) {
         this.name = name;
         this.age = age;
+        this.marks = marks;
     }
-
     talk() {
-        console.log(`hi i am ${this.name}`);
+        console.log(`Hi! i am ${this.name}`);
     }
 }
-
-let p1 = new Person ("Gillims", 69);
-let p2 = new Person ("Bloom", 12);
